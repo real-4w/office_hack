fcd968637c1f64858976256bc921f5923c41614a
Willem van der Steen <vandersw@pgtrust.co.nz>
2020-08-25 07:55:14 +1200

    Open browser script

---
+++ open_browser.py
@@ -0,0 +1,9 @@
+import webbrowser
+
+websites = [
+'https://app.slack.com/client/T39KSQ3D4/D39L42007',
+'https://github.com/real-4w/',
+]
+
+for website in websites:
+    webbrowser.open_new(website)
