a2848f13580ad32db8e1e2a7abc14b13f6188c16
Willem van der Steen <vandersw@pgtrust.co.nz>
2020-08-26 11:41:00 +1200

    WIP

--- all_scritps.py
+++ all_scritps.py
@@ -16,30 +16,19 @@ debug = yaml_data['debug']                                                #debug
 if __name__ == "__main__":                                                    #only run when this is called by itself and not imported 
     desk1 = yaml_data['desk1']
     desk2 = yaml_data['desk2']
-    
-    
-    #needs to go to yaml file
-    command_lines = [
-    "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe",
-    "C:\Program Files (x86)\Microsoft Office\Office16\OUTLOOK.EXE"
-    ]
-
-    for command_line in command_lines :
+    execs = yaml_data['execs']
+    for command_line in execs : 
         command_line = "\"" + command_line + "\""
+        if debug == True : print (command_line)
         subprocess.Popen(shlex.split(command_line))
+    time.sleep(1)                                                               #wait for execs to fire up
     for website in desk1 :
         webbrowser.open_new(website)
-
-    time.sleep(1)                                              #only 5 calls per min allowed
-
-    #needs to go to yaml file
-    command_lines = [
-    "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe",
-    ]
-    for command_line in command_lines :
-        command_line = "\"" + command_line + "\""
-        subprocess.Popen(shlex.split(command_line))
-
+    time.sleep(1)                                                               #wait for execs to fire up
+#====== WIP Need to workout how to open new virtual desktop
+    command_line = "\"" + execs[0] + "\""                                       #this is antoher browser
+    if debug == True : print (command_line)
+    subprocess.Popen(shlex.split(command_line))
+    time.sleep(1)                                                               #wait for execs to fire up
     for website in desk2 :
         webbrowser.open_new(website)
-#WIP Need to workout how to open new virtual desktop
