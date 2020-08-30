fad4b16df5f7e3450e9732c1361ff496ab7e3412
Willem van der Steen <vandersw@pgtrust.co.nz>
2020-08-30 17:54:35 +1200

    Done for the weekend

--- start_day.py
+++ start_day.py
@@ -18,11 +18,25 @@ if __name__ == "__main__":                                                    #o
     if debug == True : print(f"Number of desktops is: {desktops}.")
     if (desktops ==  1) : 
         print("Script requires at least 2 virtual desktops open.") 
+        time.sleep(5)
         exit(1)
     desk1 = yaml_data['desk1']
     desk2 = yaml_data['desk2']
     execs1 = yaml_data['execs1']
     execs2 = yaml_data['execs2']
+    w_vda.go_to_desktop_number(1)                                                   #first do desk 2
+    time.sleep(1)
+    for command_line in execs2 : 
+        command_line = "\"" + command_line + "\""                                   
+        if debug == True : 
+            print (command_line)
+        else : 
+            subprocess.Popen(shlex.split(command_line))
+    for website in desk2 :
+        if debug == False : webbrowser.open_new(website)
+    time.sleep(1)
+    w_vda.go_to_desktop_number(0)                                                   #now do desk 1
+    time.sleep(1)
     for command_line in execs1 : 
         command_line = "\"" + command_line + "\""
         if debug == True : 
@@ -32,20 +46,7 @@ if __name__ == "__main__":                                                    #o
     for website in desk1 :
         if debug == False : 
             webbrowser.open_new(website)
-#====== WIP Need to workout how to open new virtual desktop
-    time.sleep(1)
-    w_vda.go_to_desktop_number(1)
-    for command_line in execs2 : 
-        command_line = "\"" + command_line + "\""                                       #this is another browser instance
-        if debug == True : 
-            print (command_line)
-        else : 
-            subprocess.Popen(shlex.split(command_line))
-    for website in desk2 :
-        if debug == False : webbrowser.open_new(website)
     time.sleep(1)
     Desk1Badge = badge("Browser 1 URLs opened",str(len(desk1)), messagebg='green',messagecolor='black')
     Desk2Badge = badge("Browser 2 URLs opened",str(len(desk2)), messagebg='blue')
     print(Desk1Badge, Desk2Badge)
-    w_vda.go_to_desktop_number(0)
-    time.sleep(15)    
