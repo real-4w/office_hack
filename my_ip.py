9236591d2f030ea5356e348479d7d4f8411b4dc9
Willem van der Steen <vandersw@pgtrust.co.nz>
2020-09-03 12:45:24 +1200

    Added external IP address look up.

--- my_ip.py
+++ my_ip.py
@@ -1,5 +1,10 @@
 import socket    
+from requests import get
+
 hostname = socket.gethostname()    
 IPAddr = socket.gethostbyname(hostname)    
 print(f"Your Computer Name is: {hostname}")    
-print(f"Your Computer IP Address is: {IPAddr}")  
+print(f"Your local IP address is: {IPAddr}")  
+
+ip = get('https://api.ipify.org').text
+print (f"My public IP address is: {ip}")
