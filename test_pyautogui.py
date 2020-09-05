ab4cacf746912f315949c19e4657885617f7721b
Willem van der Steen <vandersw@pgtrust.co.nz>
2020-09-06 11:46:58 +1200

    WIP pyautogui

--- test_pyautogui.py
+++ test_pyautogui.py
@@ -1,2 +1,8 @@
-import pyautogui
-pyautogui.alert('This displays some text with an OK button.')
+import pyautogui as pag
+pag.alert('This displays some text with an OK button.')
+print (pag.size())
+pag.moveTo(10, 10)
+pag.PAUSE = 2
+pag.moveTo(100, 500) 
+pag.PAUSE = 2
+pag.moveTo(500, 500)
