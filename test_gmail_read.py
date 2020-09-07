45241337aaf2213bb26399b70511659e59e0c5f4
Willem van der Steen <vandersw@pgtrust.co.nz>
2020-09-08 09:57:09 +1200

    Gmail test

---
+++ test_gmail_read.py
@@ -0,0 +1,110 @@
+#==============================================================================================================
+# (c) 2020 real-4w, based on https://www.thepythoncode.com/article/reading-emails-in-python
+#==============================================================================================================
+# Make sure you have a file called "kl.yaml" in the CWD, containing variables as follows:
+#debug : True
+#username : <your email>@gmail.com
+#password : <yourpassword | yourapplicationpassword>
+#==============================================================================================================
+import imaplib, email, yaml
+from email.header import decode_header
+import webbrowser, os
+
+def ProcessYAML (yaml_file) :
+    '''This function opens the yaml file and returns the data object'''
+    with open(yaml_file) as f:
+        y_data = yaml.load(f, Loader=yaml.FullLoader)
+        debug = y_data['debug']
+        if debug == True : print("YAML file:\n", y_data)
+    return (y_data, debug) 
+yaml_data, debug = ProcessYAML('kl.yaml')                                     #yaml settings are global variables
+
+# account credentials
+username = yaml_data['username']
+password = yaml_data['password']
+
+if debug == True :
+    print("Email", username, password)
+
+# number of top emails to fetch
+N = 3
+
+# create an IMAP4 class with SSL, use your email provider's IMAP server
+imap = imaplib.IMAP4_SSL("imap.gmail.com")
+# authenticate
+imap.login(username, password)
+
+# select a mailbox (in this case, the inbox mailbox)
+# use imap.list() to get the list of mailboxes
+status, messages = imap.select("INBOX")
+
+# total number of emails
+messages = int(messages[0])
+if debug == True :
+    print(f"Number of messages: {messages}")
+for i in range(messages-4, messages-N-4, -1):
+    # fetch the email message by ID
+    res, msg = imap.fetch(str(i), "(RFC822)")
+    for response in msg:
+        if isinstance(response, tuple):
+            # parse a bytes email into a message object
+            msg = email.message_from_bytes(response[1])
+            # decode the email subject
+            subject = decode_header(msg["Subject"])[0][0]
+            if isinstance(subject, bytes):
+                # if it's a bytes, decode to str
+                subject = subject.decode()
+            # email sender
+            from_ = msg.get("From")
+            print("Subject:", subject)
+            print("From:", from_)
+            # if the email message is multipart
+            if msg.is_multipart():
+                # iterate over email parts
+                for part in msg.walk():
+                    # extract content type of email
+                    content_type = part.get_content_type()
+                    content_disposition = str(part.get("Content-Disposition"))
+                    try:
+                        # get the email body
+                        body = part.get_payload(decode=True).decode()
+                    except:
+                        pass
+                    if content_type == "text/plain" and "attachment" not in content_disposition:
+                        # print text/plain emails and skip attachments
+                        print(body)
+                    elif "attachment" in content_disposition:
+                        # download attachment
+                        filename = part.get_filename()
+                        if filename:
+                            if not os.path.isdir(subject):
+                                # make a folder for this email (named after the subject)
+                                os.mkdir(subject)
+                            filepath = os.path.join(subject, filename)
+                            # download attachment and save it
+                            open(filepath, "wb").write(part.get_payload(decode=True))
+            else:
+                # extract content type of email
+                content_type = msg.get_content_type()
+                # get the email body
+                body = msg.get_payload(decode=True).decode()
+                if content_type == "text/plain":
+                    # print only text email parts
+                    print(body)
+            if content_type == "text/html":
+                # if it's HTML, create a new HTML file and open it in browser
+                if not os.path.isdir(subject):
+                    # make a folder for this email (named after the subject)
+                    os.mkdir(subject)
+                filename = f"{subject[:50]}.html"
+                filepath = os.path.join(subject, filename)
+                # write the file
+                open(filepath, "w").write(body)
+                # open in the default browser
+                webbrowser.open(filepath)
+
+            print("="*100)
+
+# close the connection and logout
+imap.close()
+imap.logout()
