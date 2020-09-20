6aca11b3a7c66f4d236c8a362755cb624098ded8
Willem van der Steen <vandersw@pgtrust.co.nz>
2020-09-20 13:29:55 +1200

    WIP

--- waze_api.py
+++ waze_api.py
@@ -2,6 +2,15 @@
 #https://docs.python.org/3/library/logging.html
 import WazeRouteCalculator
 import logging, yaml, datetime
+import os
+from time import sleep
+#============================================================================================================================
+def screen_clear() :                                                                    # The screen clear function
+   if os.name == 'posix':                                                               # for mac and linux(here, os.name is 'posix')
+      _ = os.system('clear')
+   else:                                                                                # for windows platfrom
+      _ = os.system('cls')
+   # print out some text
 
 def ProcessYAML (yaml_file) :
     '''This function opens the yaml file and returns the data object'''
@@ -10,7 +19,7 @@ def ProcessYAML (yaml_file) :
         debug = y_data['debug']
         if debug == True : print("YAML file:\n", y_data)
     return (y_data, debug) 
-yaml_data, debug = ProcessYAML('waze_api.yaml')                                     #yaml settings are global variables
+yaml_data, debug = ProcessYAML('waze_api.yaml')                                         # yaml settings are global variables
 
 def PrintAllRoutes(all_routes, from_address, to_address) :
     print("=" * 90)
@@ -20,22 +29,25 @@ def PrintAllRoutes(all_routes, from_address, to_address) :
         if debug == True : print(key, type(all_routes[key]), all_routes[key])
         print(f"Route {key}, distance {all_routes[key][1]:.2f} km, duration {all_routes[key][0]:.2f} min.")
     return()
-if __name__ == "__main__":                                                    #only run when this is called by itself and not imported 
+#============================================================================================================================
+if __name__ == "__main__":                                                           # only run when this is called by itself and not imported 
     if debug == True :
         logger = logging.getLogger('WazeRouteCalculator.WazeRouteCalculator')
         logger.setLevel(logging.DEBUG)
         handler = logging.StreamHandler()
         logger.addHandler(handler)
-
+    
     from_address = yaml_data['from_address']
     to_address = yaml_data['to_address']
     region = yaml_data['region']
     vehicle_type = yaml_data['vehicle_type']
 
-    routes = WazeRouteCalculator.WazeRouteCalculator(from_address, to_address, region, vehicle_type)
-    all_routes_in = routes.calc_all_routes_info()
-    PrintAllRoutes(all_routes_in, from_address, to_address)
-
-    routes = WazeRouteCalculator.WazeRouteCalculator(to_address, from_address, region, vehicle_type)
-    all_routes_out = routes.calc_all_routes_info()
-    PrintAllRoutes(all_routes_out, to_address, from_address)
+    while True : 
+        routes = WazeRouteCalculator.WazeRouteCalculator(from_address, to_address, region, vehicle_type)
+        all_routes_in = routes.calc_all_routes_info()
+        PrintAllRoutes(all_routes_in, from_address, to_address)
+        routes = WazeRouteCalculator.WazeRouteCalculator(to_address, from_address, region, vehicle_type)
+        all_routes_out = routes.calc_all_routes_info()
+        PrintAllRoutes(all_routes_out, to_address, from_address)
+        sleep(60)
+        screen_clear()
